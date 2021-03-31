cebf579b41f2267a0089bb24c5eb352b71495db3
Deepak Gopalan <gopalan.d@northeastern.edu>
2021-03-31 14:22:06 -0400

    completed

--- index.js
+++ index.js
@@ -2,37 +2,37 @@ const express = require('express');
 const aws = require('aws-sdk');
 const app = express();
 const cors = require('cors');
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
+// var bodyParser = require('body-parser');
 app.use(cors());
+app.use(express.urlencoded());
+
+app.use(express.json());
 app.get('/', (req, res) => res.send('up'));
 
-app.post('/sendEmail',jsonParser, (req, res) =>{
+app.post('/sendEmail', (req, res) =>{
+    console.log("Request Received!");
 
-    let senderEmail = req.body.senderEmail;
-    let messagBody = req.body.messagBody;
-    let ARN;
+    let ARN='arn:aws:sns:us-east-1:384467288578:TOPIC_EMAIL';
     aws.config.update({ region: "us-east-1" });
+    let message = req.body;
+    console.log(message);
     let sns = new aws.SNS();
-    sns.listTopics((err, data) => {
-        if (err) 
-        console.log("err in sns listTopics : "+ err.message);
-        else 
-        ARN = data.Topics[0].TopicArn;
-
-        let params = {
+    let params = {
           TopicArn: ARN,
-          Message: `${senderEmail},${messagBody}`
+          Message: message.senderEmail+","+message.messageBody
         };
 
-        sns.publish(params, (err, data) => {
-          if (err) {
-            console.log("error in publishing message: "+ err.message);
-          } else {
-            console.log("Request recieved!");
-          }
-        });
+    sns.publish(params, (err, data) => {
+      if (err) {
+        console.log("error in publishing message: "+ err.message);
+      } 
+      if(data) {
+        console.log("Published to Topic!");
+        } 
       });
+      console.log("Request dispatched!");
+      res.setHeader("Access-Control-Allow-Origin", "*");  
+      res.json({"status":"sent"});
     });  
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
